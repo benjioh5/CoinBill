e4a91438cb848a77bd9e616b437a31ed998503a4
ArtBlnd <jjr0502@naver.com>
2017-11-07 15:43:40 +0900

    Just removing basic methods.

--- include/User/Blockv1.h
+++ include/User/Blockv1.h
@@ -5,7 +5,7 @@
 
 namespace CoinBill
 {
-    class BlockHeaderv1 final
+    struct BlockHeaderv1
     {
     
         SHA512_t    m_PrevHash;
@@ -20,35 +20,12 @@ namespace CoinBill
         uint64_t    m_CoinUsed;
         
         uint64_t    m_Nonce;
-    public:
-        void incNonce();
-        
-        void setPrevHash(SHA512_t& hash);
-        void setAuther(ACCOUNT& account);
-        void setVersion(uint64_t version);
-        void setDifficulty(uint64_t difficulty);
-        void setNumber(uint64_t number);
-        void setCoinLimit(uint64_t coin_limit);
-        void setCoinUsed(uint64_t coin_used);
-        void setNonce(uint64_t nonce);
-
-        SHA512_t& getPrevHash();
-        ACCOUNT& getAccount();
-        uint64_t getVersion() const;
-        uint64_t getDifficulty() const;
-        uint64_t getNumber() const;
-        uint64_t getCoinLimit() const;
-        uint64_t getCoinUsed() const;
-        uint64_t getNonce() const;
     };
 
     class Blockv1
     {
         BlockHeaderv1   m_Header;
         SHA512_t        m_HeaderHash;
-
-    public:
-
     };
 }
 
