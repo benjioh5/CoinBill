40332b53329a521e7247c9558485e08f628d14ef
ArtBlnd <jjr0502@naver.com>
2017-11-09 15:52:02 +0900

    Fixed whitespaces.

--- lib/Network/SocketFuncBinding.cpp
+++ lib/Network/SocketFuncBinding.cpp
@@ -57,7 +57,7 @@ namespace CoinBill
 
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
             // Winsock 2 implements. native binding.
             // if successed, socket function will return socket handle.
             if(SOCKET sock = socket(AF_INET, (int)Type, IPPROTO_TCP)) {
@@ -74,7 +74,7 @@ namespace CoinBill
 
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
             // Winsock 2 implements. native binding.
 
             // Returning zero mean successed.
@@ -86,7 +86,7 @@ namespace CoinBill
         bool SocketDisconnect(const SOCKET_HANDLE Handle) {
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
 #endif
             return true;
         }
@@ -94,21 +94,21 @@ namespace CoinBill
         bool SocketBind(const SOCKET_HANDLE Handle, const std::string& Address, short Port) {
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
 #endif
             return true;
         }
         bool SocketListen(const SOCKET_HANDLE Handle) {
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
 #endif
             return true;
         }
         bool SocketAccept(const SOCKET_HANDLE Handle, SOCKET_HANDLE& Accepted) {
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
 #endif
             return true;
         }
@@ -116,14 +116,14 @@ namespace CoinBill
         size_t SocketSend(const SOCKET_HANDLE Handle, void* pBuf, size_t& szBuf) {
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
 #endif
             return 0;
         }
         size_t SocketRecv(const SOCKET_HANDLE Handle, void* pBuf, size_t& szBuf, int timeout) {
 #ifdef COINBILL_USE_BOOST_ASIO
             // TODO : Boost implements.
-#elif COINBILL_WINDOWS
+#elif  COINBILL_WINDOWS
 #endif
             return 0;
         }
