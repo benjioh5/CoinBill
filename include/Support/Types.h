696c4a4c20be80e8be66cb8ac02111bfee3c9730
ArtBlnd <jjr0502@naver.com>
2017-11-10 09:25:56 +0900

    Miner fixes of BigType, implemented type aligner.

--- include/Support/Types.h
+++ include/Support/Types.h
@@ -8,12 +8,12 @@ namespace CoinBill
 {
     
     template <unsigned int size, class BaseTy = unsigned char, BaseTy max_size = (BaseTy)(-1)>
-    class BigInt
+    class BigTypeBase
     {
         // NOTE : DO NOT CREATE ANY VIRTUAL METHODS, OR ANY OTHER VARIABLES.
         //        THIS IS FOR STORING RAW BIG INT VARIABLES.
     protected:
-        typedef BigInt<size, BaseTy> MTy;
+        typedef BigTypeBase<size, BaseTy> MTy;
         BaseTy data[size];
 
         // I think... we can make this lot more faster tho.
@@ -105,6 +105,17 @@ namespace CoinBill
             return true;
         }
 
+        // custom type constructor.
+        template<class Ty>
+        BigTypeBase(const Ty Init) {
+            ZeroFill();
+            Ty* VTy = toType<Ty>();
+            *VTy = Init;
+        }
+        // default type constructor / distructor.
+        BigTypeBase() = default;
+        ~BigTypeBase() = default;
+
         uint8_t* toUint8(unsigned int& _size) { return toType<uint8_t>(_size); }
         uint16_t* toUint16(unsigned int& _size) { return toType<uint16_t>(_size); }
         uint32_t* toUint32(unsigned int& _size) { return toType<uint32_t>(_size); }
@@ -116,14 +127,26 @@ namespace CoinBill
         uint64_t* toUint64() { return toType<uint64_t>(); }
     };
 
-    typedef BigInt<2, uint64_t>                                     uint128_t;
-    typedef BigInt<4, uint64_t>                                     uint256_t;
-    typedef BigInt<8, uint64_t>                                     uint512_t;
-    typedef BigInt<32, uint64_t>                                    uint2048_t;
+    typedef BigTypeBase<2, uint64_t>                                uint128_t;
+    typedef BigTypeBase<4, uint64_t>                                uint256_t;
+    typedef BigTypeBase<8, uint64_t>                                uint512_t;
+    typedef BigTypeBase<32, uint64_t>                               uint2048_t;
 
     typedef uint2048_t                                              RSA2048_t;
     typedef uint256_t                                               SHA256_t;
     typedef uint512_t                                               SHA512_t;
+
+    template <class Alignee, size_t szAlign>
+    struct AlignedType {
+        typedef alignas(szAlign)Alignee Type;
+    };
+    template <class Alignee, class Aligner, size_t szAlign = sizeof(Aligner)>
+    class AlignedType {
+        typedef AlignedType<Alignee, szAlign>::Type Type;
+    };
+
+    template <class Ty>
+    using RSABlock = AlignedType<Ty, RSA2048_t>::Type;
 }
 
 #endif
